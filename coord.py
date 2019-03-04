0936cad2776ab270a396f117ecd087b85d26bd52
astrojysun <astrojysun@outlook.com>
2019-03-03 22:25:56 -0500

    extended functionality of 'coord.deproject'

--- coord.py
+++ coord.py
@@ -8,18 +8,23 @@ from astropy.coordinates import SkyCoord
 
 
 def deproject(center_coord=None, incl=0*u.deg, pa=0*u.deg,
-              header=None, wcs=None, naxis=None,
-              return_xymap=False):
+              header=None, wcs=None, naxis=None, ra=None, dec=None,
+              return_offset=False):
 
     """
-    Generate deprojected radius and projected angle maps.
+    Calculate deprojected radii and projected angles in a disk.
 
     This function deals with projected images of astronomical objects
     with an intrinsic disk geometry. Given sky coordinates of the
     disk center, disk inclination and position angle, this function
-    generates 2D maps of deprojected radius and projected angle.
-    Both quantities are defined relative to the center in the inclined
-    disk frame. The outputs are the projected maps onto the sky.
+    calculates deprojected radii and projected angles based on
+    (1) a FITS header (`header`), or
+    (2) a WCS object with specified axis sizes (`wcs` + `naxis`), or
+    (3) RA and DEC coodinates (`ra` + `dec`).
+    Both deprojected radii and projected angles are defined relative
+    to the center in the inclined disk frame. For (1) and (2), the
+    outputs are 2D images; for (3), the outputs are arrays with shapes
+    matching the broadcasted shape of `ra` and `dec`.
 
     Parameters
     ----------
@@ -37,9 +42,21 @@ def deproject(center_coord=None, incl=0*u.deg, pa=0*u.deg,
         WCS of the output 2D maps
     naxis : array-like (with two elements), optional
         Size of the output 2D maps
-    return_xymap : bool, optional
-        Whether to return the projected x-y coordinates together with
-        deprojected radius and angle maps. Default is to not return.
+    ra : array-like, optional
+        RA coordinate of the sky locations of interest
+    dec : array-like, optional
+        DEC coordinate of the sky locations of interest
+    return_offset : bool, optional
+        Whether to return the angular offset coordinates together with
+        deprojected radii and angles. Default is to not return.
+
+    Returns
+    -------
+    deprojected coordinates : list of arrays
+        If `return_offset` is set to True, the returned arrays include
+        deprojected radii, projected angles, as well as angular offset
+        coordinates along East-West and North-South direction;
+        otherwise only the former two arrays will be returned.
 
     Notes
     -----
@@ -48,22 +65,6 @@ def deproject(center_coord=None, incl=0*u.deg, pa=0*u.deg,
     https://github.com/akleroy/cpropstoo/blob/master/cubes/deproject.pro
     """
 
-    if header is not None:
-        wcs_cel = WCS(header).celestial
-        naxis1 = header['NAXIS1']
-        naxis2 = header['NAXIS2']
-    else:
-        if (wcs is None) or (naxis is None):
-            raise ValueError(
-                "If no header is provided, then both"
-                "`wcs` and `naxis` should be specified.")
-        wcs_cel = wcs.celestial
-        naxis1, naxis2 = naxis
-
-    if center_coord is None:
-        raise ValueError(
-            "`center_coord` should be either a SkyCoord object "
-            "or an array-like object with two elements.")
     if isinstance(center_coord, SkyCoord):
         x0_deg = center_coord.ra.degree
         y0_deg = center_coord.dec.degree
@@ -72,7 +73,6 @@ def deproject(center_coord=None, incl=0*u.deg, pa=0*u.deg,
         if hasattr(x0_deg, 'unit'):
             x0_deg = x0_deg.to(u.deg).value
             y0_deg = y0_deg.to(u.deg).value
-
     if hasattr(incl, 'unit'):
         incl_deg = incl.to(u.deg).value
     else:
@@ -82,33 +82,49 @@ def deproject(center_coord=None, incl=0*u.deg, pa=0*u.deg,
     else:
         pa_deg = pa
 
-    # create ra and dec grids
-    ragrid = np.arange(naxis1)
-    decgrid = np.arange(naxis2).reshape(-1, 1)
-    ramap, decmap = wcs_cel.wcs_pix2world(ragrid, decgrid, 0)
+    if header is not None:
+        wcs_cel = WCS(header).celestial
+        naxis1 = header['NAXIS1']
+        naxis2 = header['NAXIS2']
+        # create ra and dec grids
+        ix = np.arange(naxis1)
+        iy = np.arange(naxis2).reshape(-1, 1)
+        ra_deg, dec_deg = wcs_cel.wcs_pix2world(ix, iy, 0)
+    elif (wcs is not None) and (naxis is not None):
+        wcs_cel = wcs.celestial
+        naxis1, naxis2 = naxis
+        # create ra and dec grids
+        ix = np.arange(naxis1)
+        iy = np.arange(naxis2).reshape(-1, 1)
+        ra_deg, dec_deg = wcs_cel.wcs_pix2world(ix, iy, 0)
+    else:
+        ra_deg, dec_deg = np.broadcast_arrays(ra, dec)
+        if hasattr(ra_deg, 'unit'):
+            ra_deg = ra_deg.to(u.deg).value
+            dec_deg = dec_deg.to(u.deg).value
 
     # recast the ra and dec arrays in term of the center coordinates
     # arrays are now in degrees from the center
-    dxmap_deg = (ramap - x0_deg) * np.cos(np.deg2rad(y0_deg))
-    dymap_deg = decmap - y0_deg
+    dx_deg = (ra_deg - x0_deg) * np.cos(np.deg2rad(y0_deg))
+    dy_deg = dec_deg - y0_deg
 
     # rotation angle (rotate x-axis up to the major axis)
     rotangle = np.pi/2 - np.deg2rad(pa_deg)
 
     # create deprojected coordinate grids
-    deprojdx_deg = (dxmap_deg * np.cos(rotangle) +
-                    dymap_deg * np.sin(rotangle))
-    deprojdy_deg = (dymap_deg * np.cos(rotangle) -
-                    dxmap_deg * np.sin(rotangle))
+    deprojdx_deg = (dx_deg * np.cos(rotangle) +
+                    dy_deg * np.sin(rotangle))
+    deprojdy_deg = (dy_deg * np.cos(rotangle) -
+                    dx_deg * np.sin(rotangle))
     deprojdy_deg /= np.cos(np.deg2rad(incl_deg))
 
     # make map of deprojected distance from the center
-    rmap_deg = np.sqrt(deprojdx_deg**2 + deprojdy_deg**2)
+    radius_deg = np.sqrt(deprojdx_deg**2 + deprojdy_deg**2)
 
     # make map of angle w.r.t. position angle
-    amap_deg = np.rad2deg(np.arctan2(deprojdy_deg, deprojdx_deg))
+    projang_deg = np.rad2deg(np.arctan2(deprojdy_deg, deprojdx_deg))
 
-    if return_xymap:
-        return rmap_deg, amap_deg, dxmap_deg, dymap_deg
+    if return_offset:
+        return radius_deg, projang_deg, dx_deg, dy_deg
     else:
-        return rmap_deg, amap_deg
+        return radius_deg, projang_deg
